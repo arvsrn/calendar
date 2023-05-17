c086224be427e44c3dcceac49c0b11daa23b13d8
Aarav Sareen <96787824+arvsrn@users.noreply.github.com>
2023-05-17 12:39:25 +0530

    Better error handling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import { page } from "$app/stores";
+    import { onMount } from "svelte";
+    
+    onMount(() => {
+        window.location.assign(`/?error=${$page.status}`);
+    });
 </script>
 
 <main>
-    <svg style:margin-bottom="8px" width="75" height="70" viewBox="0 0 75 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1 19.16V50.84L37.5 68M1 19.16L37.5 36.32M1 19.16L10.125 14.87L14.6875 12.725L23.8125 17.015M37.5 68V36.32M37.5 68L51.1875 61.565M74 19.16V50.84L60.3125 57.275M74 19.16L37.5 2L28.375 6.29L23.8125 8.435M74 19.16L64.875 23.45L60.3125 25.595M37.5 36.32L46.625 32.03L51.1875 29.885M23.8125 8.435L60.3125 25.595M23.8125 8.435V17.015M51.1875 29.885L23.8125 17.015M51.1875 29.885V53M60.3125 25.595V57.275M60.3125 57.275L51.1875 61.565M60.3125 57.275L51.1875 53M51.1875 61.565V53" stroke="#ededed" stroke-width="2"/>
-    </svg>           
-    <h1>Error: {$page.error?.message}</h1>
-    <p>Why are you here? The app is over at <a href="">calendar.aarv.me</a>.</p>
+    <div></div>
 </main>
 
 <style>
@@ -15,7 +16,7 @@
         width: 100vw;
         height: 100vh;
 
-        background: #1c1c1c;
+        background: #141414;
 
         display: flex;
         flex-direction: column;
@@ -27,32 +28,22 @@
         position: relative;
     }
 
-    h1, p {
-        max-width: 600px;
-        text-align: center;
-    }
-
-    h1 {
-        color: #ededed;
-        font-size: 15px;
-        font-weight: bold;
-    }
-
-    @media only screen and (max-width: 857.14px) {
-        h1, p {
-            width: 70vw;
+    @keyframes donut-spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
         }
     }
 
-    p {
-        font-size: 13px;
-        color: #ededed;
-    }
-
-    a {
-        text-decoration: none;
-        background: #ededed;
-        color: #1c1c1c;
-        font-weight: 500;
+    div {
+        display: inline-block;
+        border: 4px solid rgba(255, 255, 255, 0.1);
+        border-left-color: white;
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+        animation: donut-spin 1.2s linear infinite;
     }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+    import { page } from "$app/stores";
+    import { addNotification, notifications } from "../core";
     // TODO: uncomment these lines for prod
     // import { dev } from '$app/environment';
     // import { inject } from '@vercel/analytics';
     import Toast from "./Toast.svelte";
     
     // inject({ mode: dev ? 'development' : 'production' });
+
+    notifications.set([]);
+    $page.url.searchParams.forEach((v, k) => {
+        if (k == 'error')
+            addNotification({ heading: v, description: "You were redirected to the application page." });
+    });
 </script>
 
 <slot></slot>
